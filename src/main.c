7b4dec4ba7df056a70a8969e6152bc1f82ba4a23
MEnayati <menayati@sissa.it>
2023-11-23 20:44:42 +0330

    Mohammad: main.c, the first version

---
+++ src/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+
+/* generic file- or pathname buffer length */
+#define LJMD_VERSION 1.0
+
+int main(int argc, char **argv)
+{
+    int nprint, i;
+    char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
+    FILE *fp,*traj,*erg;
+    mdsys_t sys;
+    double t_start;
+
+    printf("LJMD version %3.1f\n", LJMD_VERSION);
+
+    t_start = wallclock();
+
+    read(line, restfile, trajfile, ergfile, &sys, &nprint);
+
+    /* allocate memory */
+    sys.rx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.ry=(double *)malloc(sys.natoms*sizeof(double));
+    sys.rz=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vy=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vz=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fy=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fz=(double *)malloc(sys.natoms*sizeof(double));
+
+     /* read restart */
+    fp=fopen(restfile,"r");
+    if(fp) {
+        for (i=0; i<sys.natoms; ++i) {
+            fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
+        }
+        for (i=0; i<sys.natoms; ++i) {
+            fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
+        }
+        fclose(fp);
+        azzero(sys.fx, sys.natoms);
+        azzero(sys.fy, sys.natoms);
+        azzero(sys.fz, sys.natoms);
+    } else {
+        perror("cannot read restart file");
+        return 3;
+    }
+
+    /* initialize forces and energies.*/
+    sys.nfi=0;
+    force(&sys);
+    ekin(&sys);
+
+    erg=fopen(ergfile,"w");
+    traj=fopen(trajfile,"w");
+
+    printf("Startup time: %10.3fs\n", wallclock()-t_start);
+    printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
+    printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
+    output(&sys, erg, traj);
+
+    /* reset timer */
+    t_start = wallclock();
+
+    /**************************************************/
+    /* main MD loop */
+    for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
+
+        /* write output, if requested */
+        if ((sys.nfi % nprint) == 0)
+            output(&sys, erg, traj);
+
+        /* propagate system and recompute energies */
+        velverlet(&sys);
+        ekin(&sys);
+    }
+    /**************************************************/
+
+    /* clean up: close files, free memory */
+    printf("Simulation Done. Run time: %10.3fs\n", wallclock()-t_start);
+
+    cleanup(&sys, erg, traj);
+
+    return 0;
+}
