8b8b1dc87c2f8e56db4f9a8baf16881ee6e93161
MumbiGachomba04 <mumbigachomba254@gmail.com>
2023-11-23 17:15:30 +0100

    Regina: input,outputs and utilities functions added

---
+++ src/input.c
@@ -0,0 +1,47 @@
+#include "input.h"
+
+void read(char *line, char *restfile, char *trajfile, char *ergfile, struct mdsys_t *sys, int *nprint)
+{
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->natoms = atoi(line);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->mass = atof(line);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->epsilon = atof(line);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->sigma = atof(line);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->rcut = atof(line);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->box = atof(line);
+
+    if (get_a_line(stdin, restfile))
+        exit(1);
+    if (get_a_line(stdin, trajfile))
+        exit(1);
+    if (get_a_line(stdin, ergfile))
+        exit(1);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->nsteps = atoi(line);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    sys->dt = atof(line);
+
+    if (get_a_line(stdin, line))
+        exit(1);
+    *nprint = atoi(line);
+}
---
+++ src/input.h
@@ -0,0 +1,10 @@
+#ifndef INPUT_H
+#define INPUT_H
+#include <stdlib.h>
+#include <stdio.h>
+#include "utilities.h" // for the get_a_line function
+
+
+    void read(char *line, char *restfile, char *trajfile, char *ergfile, struct mdsys_t *sys, int *nprint);
+
+#endif
