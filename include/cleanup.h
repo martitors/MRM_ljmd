aae9134d9a75d6181cb5aa792dc9a133e845d815
martitors <martina.torsello@gmail.com>
2023-11-24 09:08:42 +0100

    Martina: all .h file, now all headers are in the include folder

---
+++ include/cleanup.h
@@ -0,0 +1,10 @@
+#ifndef CLEANUP_H
+#define CLEANUP_H
+	
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include "types.h"
+	void cleanup(mdsys_t *sys, FILE *erg, FILE *traj);
+
+
+#endif
---
+++ include/force_compute.h
@@ -0,0 +1,9 @@
+#ifndef FORCE_COMPUTE_H
+#define FORCE_COMPUTE_H
+
+    #include "types.h"
+    #include "utilities.h"
+
+    void force(mdsys_t *sys);
+
+#endif
