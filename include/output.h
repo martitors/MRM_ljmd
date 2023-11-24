51fdf171af49da18f84955c150dc08958f92f19c
MumbiGachomba04 <mumbigachomba254@gmail.com>
2023-11-24 11:28:44 +0100

    Regina: added extern c to header files input output and utilities

--- include/output.h
+++ include/output.h
@@ -1,9 +1,13 @@
 #ifndef OUTPUT_H
-#define OUTPUT_H
-#include <stdlib.h>
-#include <stdio.h>
-#include "types.h"
-
-    void output(mdsys_t *sys, FILE *erg, FILE *traj);
-
+#ifdef __cplusplus
+extern "C"{
+    #endif
+    #define OUTPUT_H
+        #include <stdlib.h>
+        #include <stdio.h>
+        #include "types.h"
+        void output(mdsys_t *sys, FILE *erg, FILE *traj);
+    #ifdef __cplusplus
+   } 
+#endif
 #endif
