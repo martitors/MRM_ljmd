71eccf3a93bfb9b530213c8fe8e5d18853b94c76
martitors <martina.torsello@gmail.com>
2023-11-24 11:17:12 +0100

    Martina: cleanup.h, add extern for cpp

--- include/cleanup.h
+++ include/cleanup.h
@@ -1,10 +1,16 @@
 #ifndef CLEANUP_H
-#define CLEANUP_H
+#ifdef __cplusplus
+extern "C" {	
+	#endif
+
+	#define CLEANUP_H
+		
+		#include <stdlib.h>
+		#include <stdio.h>
+		#include "types.h"
+		void cleanup(mdsys_t sys, FILE *erg, FILE *traj);
 	
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include "types.h"
-	void cleanup(mdsys_t sys, FILE *erg, FILE *traj);
-
-
+	#ifdef  __cplusplus
+	}
+	#endif
 #endif
