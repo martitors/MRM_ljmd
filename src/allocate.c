0ead4451cfee7241ecca5c7889f1adec1a96d721
MumbiGachomba04 <mumbigachomba254@gmail.com>
2023-11-25 10:56:50 +0100

    Everybody: pointers should be arrows and not dots

--- src/allocate.c
+++ src/allocate.c
@@ -2,14 +2,14 @@
 
 void allocate(mdsys_t *sys){
     /* allocate memory */
-    sys.rx=(double *)malloc(sys.natoms*sizeof(double));
-    sys.ry=(double *)malloc(sys.natoms*sizeof(double));
-    sys.rz=(double *)malloc(sys.natoms*sizeof(double));
-    sys.vx=(double *)malloc(sys.natoms*sizeof(double));
-    sys.vy=(double *)malloc(sys.natoms*sizeof(double));
-    sys.vz=(double *)malloc(sys.natoms*sizeof(double));
-    sys.fx=(double *)malloc(sys.natoms*sizeof(double));
-    sys.fy=(double *)malloc(sys.natoms*sizeof(double));
-    sys.fz=(double *)malloc(sys.natoms*sizeof(double));
+    sys->rx=(double *)malloc(sys->natoms*sizeof(double));
+    sys->ry=(double *)malloc(sys->natoms*sizeof(double));
+    sys->rz=(double *)malloc(sys->natoms*sizeof(double));
+    sys->vx=(double *)malloc(sys->natoms*sizeof(double));
+    sys->vy=(double *)malloc(sys->natoms*sizeof(double));
+    sys->vz=(double *)malloc(sys->natoms*sizeof(double));
+    sys->fx=(double *)malloc(sys->natoms*sizeof(double));
+    sys->fy=(double *)malloc(sys->natoms*sizeof(double));
+    sys->fz=(double *)malloc(sys->natoms*sizeof(double));
 
 }
