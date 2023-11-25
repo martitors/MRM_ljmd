18405bde7f82aa08e0634725c90289e6595dbebb
MEnayati <menayati@sissa.it>
2023-11-25 20:10:34 +0330

    Mohammad: force_compute, for kinetic energy omp parallel is added

--- src/force_compute.c
+++ src/force_compute.c
@@ -46,6 +46,10 @@ void ekin(mdsys_t *sys)
     int i;
 
     sys->ekin=0.0;
+
+#ifdef _OPENMP
+#pragma omp parallel for reduction(+ : sys->ekin)
+#endif
     for (i=0; i<sys->natoms; ++i) {
         sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
     }
