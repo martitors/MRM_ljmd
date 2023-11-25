cc7c2587080a1495d981915b91b3589a3afe2659
MumbiGachomba04 <mumbigachomba254@gmail.com>
2023-11-25 17:31:38 +0100

    Regina: optimised by avoiding expensive math.code is now 9.8x faster

--- src/force_compute.c
+++ src/force_compute.c
@@ -3,7 +3,7 @@
 
 void force(mdsys_t *sys)
 {
-    double r,ffac;
+    double ffac,c12,c6,rcsq,rsq;
     double rx,ry,rz;
     int i,j;
 
@@ -12,6 +12,10 @@ void force(mdsys_t *sys)
     azzero(sys->fx,sys->natoms);
     azzero(sys->fy,sys->natoms);
     azzero(sys->fz,sys->natoms);
+
+    c12=4.0*sys->epsilon*pow(sys->sigma,12.0);
+    c6 =4.0*sys->epsilon*pow(sys->sigma, 6.0);
+    rcsq = sys->rcut * sys->rcut;
 
     for(i=0; i < (sys->natoms) -1 ; ++i) {
         for(j=i+1; j < (sys->natoms); ++j) {
@@ -23,19 +27,18 @@ void force(mdsys_t *sys)
             rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
             ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
             rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-            r = sqrt(rx*rx + ry*ry + rz*rz);
+            rsq = rx*rx + ry*ry + rz*rz;
 
             /* compute force and energy if within cutoff */
-            if (r < sys->rcut) {
-                ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
-                                         +6*pow(sys->sigma/r,6.0)/r);
+            if (rsq < rcsq) {
+                double r6,rinv; rinv=1.0/rsq; r6=rinv*rinv*rinv;
+                ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
+                sys->epot += r6*(c12*r6 - c6);
 
-                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
-                                               -pow(sys->sigma/r,6.0));
 
-                sys->fx[i] += rx/r*ffac;          sys->fx[j] -= rx/r*ffac;
-                sys->fy[i] += ry/r*ffac;          sys->fy[j] -= ry/r*ffac;
-                sys->fz[i] += rz/r*ffac;          sys->fz[j] -= rz/r*ffac;
+                sys->fx[i] += rx*ffac;          sys->fx[j] -= rx*ffac;
+                sys->fy[i] += ry*ffac;          sys->fy[j] -= ry*ffac;
+                sys->fz[i] += rz*ffac;          sys->fz[j] -= rz*ffac;
             }
         }
     }
