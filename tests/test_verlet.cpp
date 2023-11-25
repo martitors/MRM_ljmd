6b41e668b47d0ec68928946cb4c3ba8126419804
martitors <martina.torsello@gmail.com>
2023-11-25 15:52:53 +0100

    Martina: test_verlet, memory leak fixed

--- tests/test_verlet.cpp
+++ tests/test_verlet.cpp
@@ -24,29 +24,29 @@ TEST(Verlet, TimeIntegration){
     sys1.rx[1] = 2.5; sys1.ry[1] = 0.0; sys1.rz[1] = 3.0;
 
     sys1.vx[0] = 1.0; sys1.vy[0] = 3.0; sys1.vz[0] = 0.0;
-    sys1.vx[1] = 0.0; sys1.vy[0] = 1.0; sys1.vz[1] = 2.0;
+    sys1.vx[1] = 0.0; sys1.vy[1] = 1.0; sys1.vz[1] = 2.0;
   
     sys1.fx[0] = 0.5; sys1.fy[0] = -1.0; sys1.fz[0] = 0.0;
-    sys1.fx[1] = -0.5; sys1.fy[0] = 1.0; sys1.fz[1] = 0.0;
+    sys1.fx[1] = -0.5; sys1.fy[1] = 1.0; sys1.fz[1] = 0.0;
 
     // Call the force function
 
     velverlet_1(&sys1);
 
     ASSERT_DOUBLE_EQ(sys1.rx[0],2.5004183999999725);
-    ASSERT_DOUBLE_EQ(sys1.ry[0],3.500836799999945);
+    ASSERT_DOUBLE_EQ(sys1.ry[0],7.499163200000055);
     ASSERT_DOUBLE_EQ(sys1.rz[0],2.0);
     ASSERT_DOUBLE_EQ(sys1.vx[0],1.0002091999999863);
-    ASSERT_DOUBLE_EQ(sys1.vy[0],1.0004183999999725);
+    ASSERT_DOUBLE_EQ(sys1.vy[0],2.9995816000000275);
     ASSERT_DOUBLE_EQ(sys1.vz[0],0.0);
 
     velverlet_1(&sys1);
 
     ASSERT_DOUBLE_EQ(sys1.rx[1],2.4987448000000825);
-    ASSERT_DOUBLE_EQ(sys1.ry[1],0.0);
+    ASSERT_DOUBLE_EQ(sys1.ry[1],4.002510399999835);
     ASSERT_DOUBLE_EQ(sys1.rz[1],11.0);
     ASSERT_DOUBLE_EQ(sys1.vx[1],-0.00041839999997254782);
-    ASSERT_DOUBLE_EQ(sys1.vy[1],0.0);
+    ASSERT_DOUBLE_EQ(sys1.vy[1],1.000836799999945);
     ASSERT_DOUBLE_EQ(sys1.vz[1],2.0);
 
 
