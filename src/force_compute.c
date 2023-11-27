cb8d3da7cdb835fbbd5d03737464d73824009a3a
MEnayati <menayati@sissa.it>
2023-11-28 02:28:16 +0330

    Mohammad: some progress in omp

--- src/force_compute.c
+++ src/force_compute.c
@@ -6,49 +6,48 @@
 
 void force(mdsys_t *sys)
 {
-    double ffac,c12,c6,rcsq,rsq;
-    double rx,ry,rz;
+    double ffac,rsq;
     int i,j, offset;
 
-    double *fx, *fy, *fz; // local pointers for openmp
     double epot=0.0; // needed for reduction with openmp
 
-    /* zero energy and forces */
+    // zero energy and force
     sys->epot=0.0;
     azzero(sys->fx,sys->natoms);
     azzero(sys->fy,sys->natoms);
     azzero(sys->fz,sys->natoms);
 
-    c12=4.0*sys->epsilon*pow(sys->sigma,12.0);
-    c6 =4.0*sys->epsilon*pow(sys->sigma, 6.0);
-    rcsq = sys->rcut * sys->rcut;
-
     #ifdef _OPENMP
-    #pragma omp parallel private(fx,fy,fz,i,j,rx,ry,rz,rsq,rsq_inv,r6,ffac,offset) reduction(+:epot)
+    #pragma omp parallel private(i,j,rsq,rsq_inv,r6,ffac,offset) reduction(+:epot)
     #endif
     {
-
-        // who is who
-        int tid; // thread id
-        int nthreads; // number of threads
+    	double c12=4.0*sys->epsilon*pow(sys->sigma,12.0);
+    	double c6 =4.0*sys->epsilon*pow(sys->sigma, 6.0);
+    	double rcsq = sys->rcut * sys->rcut;
+	   
+        double *fx, *fy, *fz; // local pointers for openmp
+        
+        int i, fromidx, toidx;  //interval variables
+        int tid;        // thread id
+        int nthreads;   // number of threads
 
         #ifdef _OPENMP
         	tid = omp_get_thread_num();
         	nthreads = omp_get_num_threads();
 		int rest = sys->natoms % nthreads;
-                if (nthreads < rest) {
-                	i_omp = sys->natoms/nthreads + 1;
-       		        fromidx = tid * i_omp;
-         		toidx = (tid + 1) * i_omp;
+                if (tid < rest) {
+                	i = sys->natoms/nthreads + 1;
+       		        fromidx = tid * i;
+         		toidx = (tid + 1) * i;
     		} else {
-        		i_omp = sys->natoms/nthreas;
-        		fromidx = tid * i_omp + rest;
-        		toidx = (tid + 1) * i_omp + rest;
+        		i = sys->natoms/nthreas;
+        		fromidx = tid * i + rest;
+        		toidx = (tid + 1) * i + rest;
     		}
         #else
         	tid = 0;
         	nthreads = 1;
-		i_omp = sys->natoms;
+		i = sys->natoms;
     		fromidx = 0;
     		toidx = sys->natoms;
         #endif
@@ -58,53 +57,86 @@ void force(mdsys_t *sys)
         fy = sys->fy + (tid * sys->natoms);
         fz = sys->fz + (tid * sys->natoms);
 
-        // zero energy and forces
+        // zero forces
         azzero(fx,sys->natoms);
         azzero(fy,sys->natoms);
         azzero(fz,sys->natoms);
 
-	#ifdef _OPENMP
-	#ifndef CHUNKSIZE
-	#define CHUNKSIZE 1
-	#endif
-	#pragma omp for schedule(guided,CHUNKSIZE)
-	#endif
+	double rx1, ry1, rz1;
+	double rx, ry, rz;
 
-    	for(i=fromidx; i < toidx ; ++i) {
-        	for(j=i+1; j < (sys->natoms); ++j) {
+    	//inside the box defined based on thread ID
+	for(int ii=fromidx; ii < toidx ; ++ii) {
+		rx1=sys->rx[ii];
+                ry1=sys->ry[ii];
+                rz1=sys->rz[ii];
 
-            /* particles have no interactions with themselves */
-            //if (i==j) continue;
+        	for(int jj=ii+1; jj < toidx ; ++jj) {
 
-            /* get distance between particle i and j */
-            rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
-            ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
-            rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-            rsq = rx*rx + ry*ry + rz*rz;
+            	/* particles have no interactions with themselves */
+            	//if (i==j) continue;
 
-            /* compute force and energy if within cutoff */
-            if (rsq < rcsq) {
-                double r6,rinv; rinv=1.0/rsq; r6=rinv*rinv*rinv;
-                ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
-                sys->epot += r6*(c12*r6 - c6);
+        		/* get distance between particle i and j */
+            		rx=pbc(rx1 - sys->rx[jj], 0.5*sys->box);
+            		ry=pbc(ry1 - sys->ry[jj], 0.5*sys->box);
+            		rz=pbc(rz1 - sys->rz[jj], 0.5*sys->box);
+            		rsq = rx*rx + ry*ry + rz*rz;
+
+            		/* compute force and energy if within cutoff */
+            		if (rsq < rcsq) {
+                		double r6,rinv; rinv=1.0/rsq; r6=rinv*rinv*rinv;
+                		ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
+                		epot += r6*(c12*r6 - c6);
 
 
-                sys->fx[i] += rx*ffac;          sys->fx[j] -= rx*ffac;
-                sys->fy[i] += ry*ffac;          sys->fy[j] -= ry*ffac;
-                sys->fz[i] += rz*ffac;          sys->fz[j] -= rz*ffac;
-            }
+                		sys->fx[ii] += rx*ffac;          sys->fx[jj] -= rx*ffac;
+                		sys->fy[ii] += ry*ffac;          sys->fy[jj] -= ry*ffac;
+                		sys->fz[ii] += rz*ffac;          sys->fz[jj] -= rz*ffac;
+            		}
+        	}
+    	}
+
+	//with outside the box defined based on the thread ID
+	for(int ii=fromidx; ii<toidx ; ++ii) {
+		kk = ii + toidx;
+		rx1=sys->rx[ii];
+                ry1=sys->ry[ii];
+                rz1=sys->rz[ii];
+		
+		for(int jj=kk ; jj<sys->natoms ; ++jj) {
+			/* get distance between particle i and j */
+                        rx=pbc(rx1 - sys->rx[jj], 0.5*sys->box);
+                        ry=pbc(ry1 - sys->ry[jj], 0.5*sys->box);
+                        rz=pbc(rz1 - sys->rz[jj], 0.5*sys->box);
+                        rsq = rx*rx + ry*ry + rz*rz;
+
+                        /* compute force and energy if within cutoff */
+                        if (rsq < rcsq) {
+                                double r6,rinv; rinv=1.0/rsq; r6=rinv*rinv*rinv;
+                                ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
+                                epot += r6*(c12*r6 - c6);
+
+
+                                sys->fx[ii] += rx*ffac;          sys->fx[jj] -= rx*ffac;
+                                sys->fy[ii] += ry*ffac;          sys->fy[jj] -= ry*ffac;
+                                sys->fz[ii] += rz*ffac;          sys->fz[jj] -= rz*ffac;
+                        }
+                }
         }
-    }
-	#ifdef _OPENMP
-	#pragma omp for
+
+
+
+	#if defined (_OPENMP)
+	#pragma omp barrier
 	#endif
-        for (i=0; i<sys->natoms; ++i) {
-            for (offset=sys->natoms; offset<nthreads*sys->natoms; offset+=sys->natoms) {
-                sys->fx[i] += sys->fx[offset + i];
-                sys->fy[i] += sys->fy[offset + i];
-                sys->fz[i] += sys->fz[offset + i];
-            }
-        }
+                for (i=1; i < sys->nthreads; ++i) {
+			int offs = i*sys->natoms;
+			for (int j=fromidx; j < toidx; ++j) {
+				sys->fx[i] += sys->fx[offset + i];
+                		sys->fy[i] += sys->fy[offset + i];
+                		sys->fz[i] += sys->fz[offset + i];
+            		}
+        	}
     }//
      sys->epot = epot;
 }
