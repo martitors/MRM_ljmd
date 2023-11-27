6eccbf382f63ea88931ea959a04838e7bcdd52b0
MEnayati <menayati@sissa.it>
2023-11-27 21:55:51 +0330

    Mohammad: omp first version

--- src/force_compute.c
+++ src/force_compute.c
@@ -1,11 +1,17 @@
 /* compute forces */
 #include "../include/force_compute.h"
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 void force(mdsys_t *sys)
 {
     double ffac,c12,c6,rcsq,rsq;
     double rx,ry,rz;
-    int i,j;
+    int i,j, offset;
+
+    double *fx, *fy, *fz; // local pointers for openmp
+    double epot=0.0; // needed for reduction with openmp
 
     /* zero energy and forces */
     sys->epot=0.0;
@@ -17,8 +23,55 @@ void force(mdsys_t *sys)
     c6 =4.0*sys->epsilon*pow(sys->sigma, 6.0);
     rcsq = sys->rcut * sys->rcut;
 
-    for(i=0; i < (sys->natoms) -1 ; ++i) {
-        for(j=i+1; j < (sys->natoms); ++j) {
+    #ifdef _OPENMP
+    #pragma omp parallel private(fx,fy,fz,i,j,rx,ry,rz,rsq,rsq_inv,r6,ffac,offset) reduction(+:epot)
+    #endif
+    {
+
+        // who is who
+        int tid; // thread id
+        int nthreads; // number of threads
+
+        #ifdef _OPENMP
+        	tid = omp_get_thread_num();
+        	nthreads = omp_get_num_threads();
+		int rest = sys->natoms % nthreads;
+                if (nthreads < rest) {
+                	i_omp = sys->natoms/nthreads + 1;
+       		        fromidx = tid * i_omp;
+         		toidx = (tid + 1) * i_omp;
+    		} else {
+        		i_omp = sys->natoms/nthreas;
+        		fromidx = tid * i_omp + rest;
+        		toidx = (tid + 1) * i_omp + rest;
+    		}
+        #else
+        	tid = 0;
+        	nthreads = 1;
+		i_omp = sys->natoms;
+    		fromidx = 0;
+    		toidx = sys->natoms;
+        #endif
+
+        // assign slices of force array to threads
+        fx = sys->fx + (tid * sys->natoms);
+        fy = sys->fy + (tid * sys->natoms);
+        fz = sys->fz + (tid * sys->natoms);
+
+        // zero energy and forces
+        azzero(fx,sys->natoms);
+        azzero(fy,sys->natoms);
+        azzero(fz,sys->natoms);
+
+	#ifdef _OPENMP
+	#ifndef CHUNKSIZE
+	#define CHUNKSIZE 1
+	#endif
+	#pragma omp for schedule(guided,CHUNKSIZE)
+	#endif
+
+    	for(i=fromidx; i < toidx ; ++i) {
+        	for(j=i+1; j < (sys->natoms); ++j) {
 
             /* particles have no interactions with themselves */
             //if (i==j) continue;
@@ -42,6 +95,18 @@ void force(mdsys_t *sys)
             }
         }
     }
+	#ifdef _OPENMP
+	#pragma omp for
+	#endif
+        for (i=0; i<sys->natoms; ++i) {
+            for (offset=sys->natoms; offset<nthreads*sys->natoms; offset+=sys->natoms) {
+                sys->fx[i] += sys->fx[offset + i];
+                sys->fy[i] += sys->fy[offset + i];
+                sys->fz[i] += sys->fz[offset + i];
+            }
+        }
+    }//
+     sys->epot = epot;
 }
 
 void ekin(mdsys_t *sys)
