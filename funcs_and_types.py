a6904c8427e712a216bd5c673c44894d1ef7e1c2
MumbiGachomba04 <mumbigachomba254@gmail.com>
2023-12-01 23:48:50 +0100

    Problem with read_input function

--- funcs_and_types.py
+++ funcs_and_types.py
@@ -1,8 +1,8 @@
 import ctypes
+from ctypes import byref
 import sys
 
-ljmd_lib = ctypes.CDLL('./libmdlib.so')
-
+ljmd_lib = ctypes.CDLL('./libmdlib.so') #,ctypes.RTLD_GLOBAL
 
 class MDSys(ctypes.Structure):
     _fields_ = [
@@ -35,115 +35,74 @@ class MDSys(ctypes.Structure):
         ("cz", ctypes.POINTER(ctypes.c_double)),
     ]
 
-#prototypes for c functions
-ljmd_lib.init_mpi_omp.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(mdsys_t)]
-ljmd_lib.init_mpi_omp.restype = None
+def __init__(self):
+    self.nfi=0
 
-ljmd_lib.force.argtypes = [ctypes.POINTER(mdsys_t)]
+
+#prototypes for c functions
+ljmd_lib.init_mpi_omp.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(MDSys)]
+ljmd_lib.init_mpi_omp.restype = None
+ 
+
+ljmd_lib.force.argtypes = [ctypes.POINTER(MDSys)]
 ljmd_lib.force.restype = None
 
-ljmd_lib.velverlet_1.argtypes = [ctypes.POINTER(mdsys_t)]
+ljmd_lib.velverlet_1.argtypes = [ctypes.POINTER(MDSys)]
 ljmd_lib.velverlet_1.restype = None
 
-ljmd_lib.velverlet_2.argtypes = [ctypes.POINTER(mdsys_t)]
+ljmd_lib.velverlet_2.argtypes = [ctypes.POINTER(MDSys)]
 ljmd_lib.velverlet_2.restype = None
 
-ljmd_lib.ekin.argtypes = [ctypes.POINTER(mdsys_t)]
+ljmd_lib.ekin.argtypes = [ctypes.POINTER(MDSys)]
 ljmd_lib.ekin.restype = None
 
-ljmd_lib.mpi_bcasts.argtypes = [ctypes.POINTER(mdsys_t)]
+ljmd_lib.mpi_bcasts.argtypes = [ctypes.POINTER(MDSys)]
 ljmd_lib.mpi_bcasts.restype = None
 
-ljmd_lib.mpi_fin.argtypes = [ctypes.POINTER(mdsys_t)]
+ljmd_lib.mpi_fin.argtypes = [ctypes.POINTER(MDSys)]
 ljmd_lib.mpi_fin.restype = None
 
-ljmd_lib.cleanup.argtypes = [mdsys_t, ctypes.POINTER(ctypes.c_void_p), ctypes.POINTER(ctypes.c_void_p)]
+ljmd_lib.cleanup.argtypes = [MDSys, ctypes.POINTER(ctypes.c_void_p), ctypes.POINTER(ctypes.c_void_p)]
 ljmd_lib.cleanup.restype = None
 
-def ekin(pysys):
+def ekinetic(pysys):
     ljmd_lib.ekin(ctypes.byref(pysys))
 
-def velverlet_1(pysys):
+def verlet_1(pysys):
     ljmd_lib.velverlet_1(ctypes.byref(pysys))
 
-def velverlet_2(pysys):
+def verlet_2(pysys):
     ljmd_lib.velverlet_2(ctypes.byref(pysys))
 
-def force(pysys):
+def force_compute(pysys):
     ljmd_lib.force(ctypes.byref(pysys))
 
-def init_mpi_omp(argc, argv, pysys):
+def initialise_mpi_omp(argc, argv, pysys):
     ljmd_lib.init_mpi_omp(argc, argv, ctypes.byref(pysys))
 
-def mpi_bcasts(pysys):
+def bcasts(pysys):
     ljmd_lib.mpi_bcasts(ctypes.byref(pysys))
 
-def cleanup(sys, erg, traj):
+def clean(sys, erg, traj):
     ljmd_lib.cleanup(sys, ctypes.byref(erg), ctypes.byref(traj))
 
-def mpi_fin(pysys):
+def mpi_finalise(pysys):
     ljmd_lib.mpi_fin(ctypes.byref(pysys))
 
-def get_a_line(ifile) :
-  line = ifile.readline()
-  line = line.partition('#')[0]
-  line = line.rstrip()
-  return line
+def read_file(line,restfile, trajfile, ergfile, pysys, nprint):
+    ljmd_lib.read_input(line,restfile, trajfile, ergfile,ctypes.byref(pysys),nprint)
 
-def read_input(line, restfile, trajfile, ergfile, pysys, nprint):
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.natoms = int(line)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.mass = float(line)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.epsilon = float(line)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.sigma = float(line)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.rcut = float(line)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.box = float(line)
-
-    if get_a_line(sys.stdin, restfile):
-        exit(1)
-    if get_a_line(sys.stdin, trajfile):
-        exit(1)
-    if get_a_line(sys.stdin, ergfile):
-        exit(1)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.nsteps = int(line)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    pysys.dt = float(line)
-
-    if get_a_line(sys.stdin, line):
-        exit(1)
-    nprint[0] = int(line)
 
 def allocate(pysys):
-    pysys.rx = (ct.c_double * pysys.natoms)()
-    pysys.ry = (ct.c_double * pysys.natoms)()
-    pysys.rz = (ct.c_double * pysys.natoms)()
-    pysys.vx = (ct.c_double * pysys.natoms)()
-    pysys.vy = (ct.c_double * pysys.natoms)()
-    pysys.vz = (ct.c_double * pysys.natoms)()
-    pysys.fx = (ct.c_double * pysys.natoms)()
-    pysys.fy = (ct.c_double * pysys.natoms)()
-    pysys.fz = (ct.c_double * pysys.natoms)()
+    pysys.rx = (ctypes.c_double * pysys.natoms)()
+    pysys.ry = (ctypes.c_double * pysys.natoms)()
+    pysys.rz = (ctypes.c_double * pysys.natoms)()
+    pysys.vx = (ctypes.c_double * pysys.natoms)()
+    pysys.vy = (ctypes.c_double * pysys.natoms)()
+    pysys.vz = (ctypes.c_double * pysys.natoms)()
+    pysys.fx = (ctypes.c_double * pysys.natoms)()
+    pysys.fy = (ctypes.c_double * pysys.natoms)()
+    pysys.fz = (ctypes.c_double * pysys.natoms)()
 
 def output(pysys, erg, traj):
     print("% 8d % 20.8f % 20.8f % 20.8f % 20.8f\n" % (pysys.nfi, pysys.temp, pysys.ekin, pysys.epot, pysys.ekin+pysys.epot))
--- ljmd.py
+++ ljmd.py
@@ -1,5 +1,8 @@
 import ctypes
+#from ctypes import byref
+import funcs_and_types as ft
 import sys
+import timeit
 
 
 if __name__ == "__main__":
@@ -7,9 +10,71 @@ if __name__ == "__main__":
     arguments = [ctypes.c_char_p(arg.encode('utf-8')) for arg in sys.argv[1:]]  # Encode string arguments as bytes
     
     # Create an instance of mdsys_t
-    pysys = mdsys_t()
-    
+    pysys = ft.MDSys()
+    print("ok1")
     # Pass the arguments and mdsys_t instance by reference to the function
-    initialize_mpi_omp(ctypes.c_int(len(arguments)), (ctypes.c_char_p * len(arguments))(*arguments), pysys)
+    ft.initialise_mpi_omp(ctypes.c_int(len(arguments)), (ctypes.c_char_p * len(arguments))(*arguments), pysys)
+    print("ok2")
+    line=None 
+    restfile = None
+    trajfile = None
+    ergfile = None
+    nprint = None
+    start=None
 
-    if pysys.
+    if pysys.rank == 0:
+        version = 1.0
+        print("LJMD version " + str(version) )
+        start = timeit.default_timer()
+        ft.read_file(line,restfile, trajfile, ergfile, pysys, nprint)
+        print("ok3")
+    
+    ft.bcasts(pysys)
+    print("ok4")
+    ft.allocate(pysys)
+    print("ok5")
+
+    if pysys.rank == 0:
+        # Read restart
+        restPath = f"../examples/{restfile}"
+
+        try:
+            with open(restPath, "r") as fp:
+                for i in range(pysys.natoms):
+                    pysys.rx[i], pysys.ry[i], pysys.rz[i] = map(float, fp.readline().split())
+                for i in range(pysys.natoms):
+                    pysys.vx[i], pysys.vy[i], pysys.vz[i] = map(float, fp.readline().split())
+        except IOError as e:
+            print("cannot read restart file:", e)
+            sys.exit(3)
+    
+    pysys.nfi=0
+    ft.force_compute(pysys)
+
+    if pysys.rank == 0:
+        ft.ekinetic(pysys)
+
+        ergPath = f"../examples/{ergfile}"
+        trajPath = f"../examples/{trajfile}"
+
+        with open(ergPath, "w") as erg, open(trajPath, "w") as traj:
+            print(f"Startup time: {timeit.default_timer() - start:10.3f}s") ## ------time diff taken
+            print(f"Starting simulation with {pysys.natoms} atoms for {pysys.nsteps} steps.")
+            print("     NFI            TEMP            EKIN                 EPOT              ETOT")
+            ft.output(pysys, erg, traj)
+            start = timeit.default_timer()
+
+    for pysys.nfi in range(1, pysys.nsteps + 1): 
+        if pysys.nfi % nprint == 0 and pysys.rank == 0:
+             ft.output(pysys, erg, traj)
+
+        ft.verlet_1(pysys)
+        ft.force_compute(pysys)
+        ft.verlet_2(pysys)
+        ft.ekinetic(pysys)
+    
+    if pysys.rank == 0:
+        print(f"Simulation Done. Run time:{timeit.default_timer() - start:10.3f}s")
+    
+    ft.clean(pysys, erg, traj)
+    ft.mpi_finalise(pysys)
